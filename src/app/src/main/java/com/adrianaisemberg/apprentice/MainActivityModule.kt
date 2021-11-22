6f6e8a840ead6c36e3ed9aeb31e75ee9f929a939
Adrian Aisemberg <adrianaisemberg@gmail.com>
2021-11-21 23:44:15 -0800

    wire search

--- src/app/src/main/java/com/adrianaisemberg/apprentice/MainActivityModule.kt
+++ src/app/src/main/java/com/adrianaisemberg/apprentice/MainActivityModule.kt
@@ -1,6 +1,7 @@
 package com.adrianaisemberg.apprentice
 
 import android.app.Activity
+import com.adrianaisemberg.apprentice.service.API
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,6 +12,12 @@ import dagger.hilt.android.components.ActivityComponent
 object MainActivityModule {
 
     @Provides
-    fun provideMainActivityViewModel(activity: Activity): MainActivityViewModel =
-        MainActivityViewModel(activity)
+    fun provideMainActivityViewModel(
+        activity: Activity,
+        api: API,
+    ): MainActivityViewModel =
+        MainActivityViewModel(
+            activity = activity,
+            api = api,
+        )
 }
--- src/app/src/main/java/com/adrianaisemberg/apprentice/MainActivityViewModel.kt
+++ src/app/src/main/java/com/adrianaisemberg/apprentice/MainActivityViewModel.kt
@@ -2,10 +2,18 @@ package com.adrianaisemberg.apprentice
 
 import android.app.Activity
 import com.adrianaisemberg.apprentice.mvvm.ActivityViewModel
+import com.adrianaisemberg.apprentice.service.API
+import com.adrianaisemberg.apprentice.service.enqueue
 
-class MainActivityViewModel(activity: Activity) : ActivityViewModel(activity),
+class MainActivityViewModel(
+    activity: Activity,
+    private val api: API,
+) : ActivityViewModel(activity),
     OnTextChangedListener {
 
     override fun onTextChanged(text: String) {
+        api.searchImages(text).enqueue {
+            val results = it.body()
+        }
     }
 }
